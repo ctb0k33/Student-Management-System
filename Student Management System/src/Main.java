abc55dc0ed70218d19d6a172628b5d2d9af7c3d6
ctb0k33 <ctb0k33@gmail.com>
2022-07-22 01:24:05 +0700

    Change file name

---
+++ Student Management System/src/Main.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		Menu menu= new Menu();
+		menu.printMenu();
+		menu.implement();
+	}
+}
